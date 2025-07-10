dd43f3cceff87e4e4c01694e5515b164b0a41a29
IzumiQuan <2180928212@qq.com>
2025-07-10 10:48:09 +0800

    Update LoginPanel.vue

--- src/components/LoginPanel.vue
+++ src/components/LoginPanel.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-import { login, remove, set, query } from '../api/userApi'
+import { login, getAvatar, remove, set, query } from '../api/userApi'
 import router from '@/router'
 import { ElNotification } from 'element-plus'
 let user = ref({
@@ -9,62 +9,74 @@ let user = ref({
 })
 let checked = ref(false)
 let data = ref({})
-let code = ref({})
 let msg = ref({})
+let avatar = ref({})
 async function handleLogin() {
-    if(!checked.value){
+    if (!checked.value) {
         ElNotification({
             title: 'Error',
             message: '请先勾选用户协议',
             type: 'error',
         })
-    }else if(user.value.tel === '' || user.value.pwd === ''){
+    } else if (user.value.tel === '' || user.value.pwd === '') {
         ElNotification({
             title: 'Error',
             message: '用户名或密码不能为空',
             type: 'error',
         })
-    }else{
-        let resp = await login(user.value)
-        
-        code = resp.code
-        if(code == 200){
-            data = resp.data
-            if(data !== null){
-                router.push("/class")//跳转页面
-            }else{
+    } else {
+        try {
+            let resp = await login(user.value)
+            if (resp.success === true) {
+                data = resp.data
+                if (data !== null) {
+                    router.push("/class")//跳转页面
+                } else {
+                    ElNotification({
+                        title: 'Success',
+                        message: '注册成功',
+                        type: 'success',
+                    })
+                }
+            } else {
+                msg = resp.msg
                 ElNotification({
-                    title: 'Success',
-                    message: '注册成功',
-                    type: 'success',
-                })
-            }
-        }else{
-            msg = resp.msg
-            ElNotification({
                     title: 'Error',
                     message: msg,
                     type: 'error',
+                })
+            }
+        } catch (e) {
+            ElNotification({
+                title: 'Error',
+                message: '无法连接服务器',
+                type: 'error',
             })
         }
     }
 }
+async function handleGetAvatar(){
+    if(user.value.tel !== ''){
+        let resp = await getAvatar(user.value)
+        avatar.value = resp.data.avatar
+    }
+}
 </script>
 
-<template> 
+<template>
     <div class="panel">
         <div class="box">
-            <el-avatar icon="el-icon-user" class="avatar" />
-            <el-input placeholder="请输入手机号" v-model="user.tel" clearable class="input" />
+            <el-avatar icon="el-icon-user" :src="avatar" class="avatar" />
+            <el-input placeholder="请输入手机号" v-model="user.tel" @blur="handleGetAvatar" clearable class="input" />
             <el-input placeholder="请输入密码" v-model="user.pwd" type="password" clearable show-password class="input" />
             <el-checkbox label="同意用户协议" v-model="checked" class="checkbox" />
-            <el-button color="black" size="large" @click.prevent="handleLogin" class="btn" >登录</el-button>
+            <el-button color="black" size="large" @click.prevent="handleLogin" class="btn">登录</el-button>
         </div>
     </div>
 </template>
 
-<style scoped> 
-.panel { 
+<style scoped>
+.panel {
     width: 500px;
     text-align: left;
     background-color: white;
