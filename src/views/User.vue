d1b6b0eaaa45f7a1ac9cc13d5a91e2b477937902
IzumiQuan <2180928212@qq.com>
2025-07-11 00:03:12 +0800

    Create User.vue

---
+++ src/views/User.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AsideBar from '@/components/AsideBar.vue';
+
+</script>
+
+<template>
+    <AsideBar />
+    <router-view></router-view>
+</template>
+
+<style scoped>
+
+</style>
