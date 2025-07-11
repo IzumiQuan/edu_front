24e097d1d3963049a0df9a250cea590e6579644c
IzumiQuan <2180928212@qq.com>
2025-07-12 00:46:28 +0800

    update api

--- src/api/classApi.js
+++ src/api/classApi.js
@@ -1,14 +1,14 @@
 import request from "@/utils/request"
-let url = "http://localhost:8080/api/course"
+let prefix = "/course"
 export async function add(data) {
-    return await request.post(url + "/add", data)
+    return await request.post(prefix + "/add", data)
 }
 export async function remove(data) {
-    return await request.post(url + "/remove/" + data)
+    return await request.post(prefix + "/remove/" + data)
 }
 export async function set(data) {
-    return await request.post(url + "/set", data)
+    return await request.post(prefix + "/set", data)
 }
 export async function query(data) {
-    return await request.post(url + "/query", data)
+    return await request.post(prefix + "/query", data)
 }
--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,21 +1,21 @@
 import request from "@/utils/request"
-let url = "http://localhost:8080/api/user"
+let prefix = "/user"
 export async function regist(data) {
     console.log(url)
-    return await request.post(url + "/regist", data)
+    return await request.post(prefix + "/regist", data)
 }
 export async function login(data) {
-    return await request.post(url + "/login", data)
+    return await request.post(prefix + "/login", data)
 }
 export async function getAvatar(data) {
-    return await request.post(baseUrl + "/avatar", data)
+    return await request.post(prefix + "/avatar", data)
 }
 export async function remove(data) {
-    return await request.post(baseUrl + "/remove/" + data)
+    return await request.post(prefix + "/remove/" + data)
 }
 export async function set(data) {
-    return await request.post(baseUrl + "/set", data)
+    return await request.post(prefix + "/set", data)
 }
 export async function query(data) {
-    return await request.post(baseUrl + "/query", data)
+    return await request.post(prefix + "/query", data)
 }
