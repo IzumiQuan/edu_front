300c3dfb9396269e6301138d76463ff257c73745
IzumiQuan <2180928212@qq.com>
2025-07-11 10:21:38 +0800

    Update classApi.js

--- src/api/classApi.js
+++ src/api/classApi.js
@@ -1,2 +1,5 @@
 import request from "@/utils/request"
 let baseUrl = "http://localhost:8080/api/class"
+export async function query(data) {
+    return await request.post(baseUrl + "/query", data)
+}
