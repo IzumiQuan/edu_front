88a44a9402bd7686e92b2dc2c635fcc0c46d5065
IzumiQuan <2180928212@qq.com>
2025-07-13 00:43:51 +0800

    Update Info.vue

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,36 +1,11 @@
 <script setup>
 import { ref, computed } from 'vue'
 
-// 响应式数据
-const userInfo = ref({
-  username: '张三',
-  role: '用户',
-  gender: '男',
-  phone: '13012345203',
-  idCard: '310101199001011234',
-  email: '2423565515@qq.com',
-  company: '单位1'
-})
+let user = ref(JSON.parse(sessionStorage.getItem('user')))
 
 const isEditModalOpen = ref(false)
 const currentEditField = ref('')
 const editValue = ref('')
-
-// 计算属性
-const avatarInitials = computed(() => {
-  return userInfo.value.username.charAt(0).toUpperCase()
-})
-
-// 方法
-const formatPhoneNumber = (phone) => {
-  if (!phone) return ''
-  return phone.replace(/(\d{3})\d{4}(\d{4})/, '$1****$2')
-}
-
-const formatIdCard = (idCard) => {
-  if (!idCard) return ''
-  return idCard.replace(/(\d{6})\d{8}(\d{4})/, '$1********$2')
-}
 
 const openEditModal = (field) => {
   currentEditField.value = field
@@ -46,80 +21,75 @@ const saveEdit = () => {
   // 这里可以添加表单验证逻辑
   userInfo.value[currentEditField.value] = editValue.value
   closeEditModal()
-  
+
   // 模拟保存到服务器
   console.log(`保存 ${currentEditField.value}:`, editValue.value)
 }
 
 const viewIdCard = () => {
-  alert(`身份证号码: ${formatIdCard(userInfo.value.idCard)}`)
+  
 }
 
-import{ useRouter } from 'vue-router'
-let router = useRouter()
-function editolral() {
-  //页面跳转
-  router.push('/edit')
-}
 </script>
 
 <template>
   <div class="profile-container">
     <!-- 头像及基本信息区域 -->
     <div class="profile-header">
-      <div class="avatar">{{ getAvatarInitials }}</div>
+      <el-avatar class="avatar" :src="user.avatar"></el-avatar>
       <div class="user-info">
         <h2 class="username">
-          {{ userInfo.username }}
-          <el-image class="edit-icon" @click="editolral" src="https://cdn8.axureshop.com/demo2025/2328743/images/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E5%9F%BA%E6%9C%AC%E4%BF%A1%E6%81%AF/u2950.svg" :fit="fit" />
+          {{ user.name }}
+          <el-image class="edit-icon"
+            src="https://cdn8.axureshop.com/demo2025/2328743/images/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E5%9F%BA%E6%9C%AC%E4%BF%A1%E6%81%AF/u2950.svg"
+            :fit="fit" />
         </h2>
         <div class="tags">
-          <span class="tag">{{ userInfo.role }}</span>
-          <span class="tag">{{ userInfo.gender }}</span>
+          <span class="tag">{{ user.role }}</span>
+          <span class="tag">{{ user.sex }}</span>
         </div>
       </div>
     </div>
-    
+
     <!-- 信息列表区域 -->
     <table class="info-table">
-      <tr>
-        <td class="label">手机号码</td>
-        <td>{{ formatPhoneNumber(userInfo.phone) }}</td>
-      </tr>
-      <tr>
-        <td class="label">身份证</td>
-        <td>
-          已认证 
-          <button class="btn" @click="openEditModal('idCard')">编辑</button>
-          <button class="btn" @click="viewIdCard">查看</button>
-        </td>
-      </tr>
-      <tr>
-        <td class="label">邮箱</td>
-        <td>{{ userInfo.email }}</td>
-      </tr>
-      <tr>
-        <td class="label">工作单位</td>
-        <td>{{ userInfo.company }}</td>
-      </tr>
+      <tbody>
+        <tr>
+          <td class="label">手机号码</td>
+          <td>{{ user.tel }}</td>
+        </tr>
+        <tr>
+          <td class="label">身份证</td>
+          <td>
+            已认证
+            <button class="btn" @click="openEditModal('idCard')">编辑</button>
+            <button class="btn" @click="viewIdCard">查看</button>
+          </td>
+        </tr>
+        <tr>
+          <td class="label">邮箱</td>
+          <td>{{ user.email }}</td>
+        </tr>
+        <tr>
+          <td class="label">工作单位</td>
+          <td>{{ user.company }}</td>
+        </tr>
+      </tbody>
     </table>
   </div>
-    
-    <!-- 编辑弹窗 -->
-    <div v-if="isEditModalOpen" class="edit-modal">
-      <div class="modal-content">
-        <h3>{{ currentEditField === 'username' ? '修改用户名' : '修改身份证' }}</h3>
-        <input 
-          v-model="editValue" 
-          :placeholder="currentEditField === 'username' ? '请输入用户名' : '请输入身份证号'" 
-          class="input-field"
-        >
-        <div class="modal-actions">
-          <button class="btn cancel-btn" @click="closeEditModal">取消</button>
-          <button class="btn save-btn" @click="saveEdit">保存</button>
-        </div>
+
+  <!-- 编辑弹窗 -->
+  <div v-if="isEditModalOpen" class="edit-modal">
+    <div class="modal-content">
+      <h3>{{ currentEditField === 'username' ? '修改用户名' : '修改身份证' }}</h3>
+      <input v-model="editValue" :placeholder="currentEditField === 'username' ? '请输入用户名' : '请输入身份证号'"
+        class="input-field">
+      <div class="modal-actions">
+        <button class="btn cancel-btn" @click="closeEditModal">取消</button>
+        <button class="btn save-btn" @click="saveEdit">保存</button>
       </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
@@ -153,13 +123,10 @@ body {
 .avatar {
   width: 80px;
   height: 80px;
-  background-color: #ccc;
-  border-radius: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 18px;
-  color: #fff;
   margin-right: 15px;
 }
 
@@ -196,7 +163,7 @@ body {
 }
 
 .info-table tr {
-  border-bottom: 1px solid #eee;
+  border: 1px solid rgba(240, 240, 240, 1);
 }
 
 .info-table td {
@@ -209,7 +176,7 @@ body {
 }
 
 .btn {
-  background-color: #fff;
+  background-color: white;
   border: 1px solid #ccc;
   padding: 6px 12px;
   border-radius: 4px;
@@ -267,4 +234,4 @@ body {
   color: white;
   border: none;
 }
-</style>  
+</style>
