ca6d3085a92ee2b86b368dcfa89161fb88125da1
IzumiQuan <2180928212@qq.com>
2025-07-12 19:15:52 +0800

    Create markingApi.js

---
+++ src/api/markingApi.js
@@ -0,0 +1,14 @@
+import request from "@/utils/request"
+let prefix = "/marking"
+export async function add(data) {
+    return await request.post(prefix + "/add", data)
+}
+export async function remove(data) {
+    return await request.post(prefix + "/remove/" + data)
+}
+export async function set(data) {
+    return await request.post(prefix + "/set", data)
+}
+export async function query(data) {
+    return await request.post(prefix + "/query", data)
+}
