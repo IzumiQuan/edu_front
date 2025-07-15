a9f51dcd7251a0c1f9be7e26829338adc39bc267
IzumiQuan <2180928212@qq.com>
2025-07-15 09:30:14 +0800

    Create EnrollLog.vue

---
+++ src/views/EnrollLog.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="container">
+    <div class="item">
+
+    </div>
+</div>
+</template>
+
+<style scoped>
+.container {
+  background-color: white;
+  width: 75%;
+  margin: 10px 5px;
+  padding: 25px 50px;
+  display: flex;
+  flex-direction: column;
+}
+.item {
+    flex: 0 0 100px;
+    padding: 15px;
+    border-bottom: 1px solid rgba(230, 230, 230, 1);
+}
+</style>
