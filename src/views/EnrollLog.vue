7fcd55e70b5aac9c675d64da86aae0361cdbf775
IzumiQuan <2180928212@qq.com>
2025-07-15 10:02:38 +0800

    Update EnrollLog.vue

--- src/views/EnrollLog.vue
+++ src/views/EnrollLog.vue
@@ -1,14 +1,30 @@
 <script setup>
-
+import { ref, onBeforeMount } from 'vue'
+import request from '@/utils/request'
+let events = ref([])
+let sc = ref({
+        example: { 
+            name: '',
+        }
+    })
+async function getEvents() { 
+    let resp = await request.post('/enroll/query', sc.value)
+    if(resp.code == 200){
+        events.value = resp.data.records
+    }
+}
+onBeforeMount(() => { 
+    getEvents()
+})
 </script>
 
 <template>
 <div class="container">
-    <div class="item">
+    <div class="item" v-for="(event, index) in events" :key="index">
         <div class="left">
-            <div style="margin: 15px 0;">活动名称</div>
-            <div>活动时间</div>
-            <div>报名时间</div>
+            <div style="margin: 15px 0;">{{ event.activity }}</div>
+            <div>活动时间：{{ new Date(event.activityDate).toLocaleString('zh-CN') }}</div>
+            <div>报名时间：{{ new Date(event.createDate).toLocaleString('zh-CN') }}</div>
         </div>
         <div class="right">
             <div>已报名</div>
